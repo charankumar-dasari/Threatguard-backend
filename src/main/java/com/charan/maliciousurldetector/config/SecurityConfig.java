71b5d543d7b03c298196d612f82ea8283ccde743
charankumardasari.it@gmail.com <charankumardasari.it@gmail.com>
2026-06-13 19:52:47 +0530

    Fix CORS for deployed frontend

--- src/main/java/com/charan/maliciousurldetector/config/SecurityConfig.java
+++ src/main/java/com/charan/maliciousurldetector/config/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .cors(cors -> cors.disable())
                 .csrf(csrf -> csrf.disable())
 
                 .authorizeHttpRequests(auth -> auth
