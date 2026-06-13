f6159bc573ebc286f1a84273644e74597036a485
charankumardasari.it@gmail.com <charankumardasari.it@gmail.com>
2026-06-13 21:33:27 +0530

    Add default admin account

--- src/main/java/com/charan/maliciousurldetector/config/DataSeeder.java
+++ src/main/java/com/charan/maliciousurldetector/config/DataSeeder.java
@@ -1,22 +1,34 @@
 package com.charan.maliciousurldetector.config;
 
 import com.charan.maliciousurldetector.entity.MaliciousUrl;
+import com.charan.maliciousurldetector.entity.Role;
+import com.charan.maliciousurldetector.entity.User;
 import com.charan.maliciousurldetector.repository.MaliciousUrlRepository;
+import com.charan.maliciousurldetector.repository.UserRepository;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DataSeeder implements CommandLineRunner {
 
     private final MaliciousUrlRepository maliciousUrlRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public DataSeeder(MaliciousUrlRepository maliciousUrlRepository) {
+    public DataSeeder(MaliciousUrlRepository maliciousUrlRepository,
+                      UserRepository userRepository,
+                      PasswordEncoder passwordEncoder) {
         this.maliciousUrlRepository = maliciousUrlRepository;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public void run(String... args) {
+
+        createAdminIfNotExists();
 
         addUrlIfNotExists(
                 "http://fake-bank-login.com",
@@ -35,6 +47,31 @@ public class DataSeeder implements CommandLineRunner {
                 "CREDENTIAL_THEFT",
                 "Fake PayPal password update page"
         );
+    }
+
+    private void createAdminIfNotExists() {
+
+        String adminEmail = "admin@threatguard.com";
+
+        User admin = userRepository.findByEmail(adminEmail).orElse(null);
+
+        if (admin == null) {
+
+            User newAdmin = new User();
+            newAdmin.setFullName("ThreatGuard Admin");
+            newAdmin.setEmail(adminEmail);
+            newAdmin.setPassword(passwordEncoder.encode("admin123"));
+            newAdmin.setRole(Role.ADMIN);
+
+            userRepository.save(newAdmin);
+
+        } else {
+
+            admin.setRole(Role.ADMIN);
+            admin.setPassword(passwordEncoder.encode("admin123"));
+
+            userRepository.save(admin);
+        }
     }
 
     private void addUrlIfNotExists(String url, String threatType, String description) {
